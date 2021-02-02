b4a29fb0e747fe0f8406ce4429369767e456fde4
sanjaya <na5703hansitha@gmail.com>
2021-02-02 15:37:31 +0530

    initial commit

---
+++ react-scripts/src/views/IndexView/components/Hero/Hero.js
@@ -0,0 +1,169 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import { Button, Typography } from '@material-ui/core';
+import { SectionHeader, TypedText } from 'components/molecules';
+import { HeroShaped } from 'components/organisms';
+
+const useStyles = makeStyles(theme => ({
+  fontWeight900: {
+    fontWeight: 900,
+  },
+  leftSideContent: {
+    '& .section-header__cta-container': {
+      [theme.breakpoints.down('xs')]: {
+        flexDirection: 'column',
+        '& .section-header__cta-item-wrapper': {
+          width: '100%',
+          '&:last-child': {
+            marginLeft: 0,
+            marginTop: theme.spacing(1),
+          },
+          '& .MuiButtonBase-root': {
+            width: '100%',
+          },
+        },
+      },
+    }
+  },
+  heroShaped: {
+    '& .hero-shaped__image': {
+      backgroundColor: theme.palette.alternate.main,
+    },
+    [theme.breakpoints.down('sm')]: {
+      '& .hero-shaped__image': {
+        position: 'relative',
+      },
+      '& .hero-shaped__wrapper': {
+        flexDirection: 'column',
+      },
+    },
+  },
+  imageAnimation: {
+    background: `url("https://assets.maccarianagency.com/the-front/web-screens/home/home-hero-bg-light.png")`,
+    backgroundRepeat: 'repeat',
+    backgroundAttachment: 'scroll',
+    backgroundSize: '400px auto',
+    animation: `$slideshow 50s linear infinite`,
+    width: '600%',
+    height: '600%',
+    backgroundColor: theme.palette.alternate.dark,
+    top: '-25%',
+    left: '-100%',
+    position: 'absolute',
+    [theme.breakpoints.up('sm')]: {
+      backgroundSize: '800px auto',
+    }
+  },
+  imageAnimationDark: {
+    background: `url("https://assets.maccarianagency.com/the-front/web-screens/home/home-hero-bg-dark.png")`,
+  },
+  '@keyframes slideshow': {
+    '0%': {
+      transform: 'rotate(-13deg) translateY(-25%)',
+    },
+    '100%': {
+      transform: 'rotate(-13deg) translateY(-80%)',
+    },
+  },
+}));
+
+const Hero = ({ themeMode = 'light', className, ...rest }) => {
+  const classes = useStyles();
+
+  const title = (
+      <div>
+          <Typography variant="h2" component="span" className={classes.fontWeight900}>
+              Transform Like the Best.
+              <br/>
+          </Typography>
+          <Typography variant="h4" component="span" className={classes.fontWeight900}>
+              Boost efficiency & reduce cost through Cloud:
+              <br/>
+              <TypedText
+                  component="span"
+                  variant="h4"
+                  color="secondary"
+                  className={classes.fontWeight900}
+                  typedProps={{
+                      strings: [
+                          'Data Warehouse',
+                          'Business Intelligence',
+                          'Remote and Real Time Collaboration',
+                      ],
+                      typeSpeed: 50,
+                      loop: true,
+                  }}
+              />
+          </Typography>
+      </div>
+
+  );
+
+  //this value is removed
+    //  'TheFront will make your product look modern and professional while saving you precious time.'
+  const subtitle = '';
+
+  const docsButton = (
+    <Button size="large" variant="outlined" color="primary" component="a" href="/documentation">
+      Documentation
+    </Button>
+  );
+
+  const buyButton = (
+    <Button
+      size="large"
+      variant="contained"
+      color="primary"
+      component="a"
+      href="/home"
+    >
+      Get started
+    </Button>
+  );
+
+  const leftSideContent = (
+    <SectionHeader
+      title={title}
+      subtitle={subtitle}
+      align="left"
+      titleProps={{
+        variant: 'h2',
+        color: 'textPrimary',
+      }}
+      data-aos="fade-right"
+      disableGutter
+      className={classes.leftSideContent}
+    />
+  );
+  return (
+    <div className={className} {...rest}>
+      <HeroShaped
+        className={classes.heroShaped}
+        leftSide={leftSideContent}
+        rightSide={(
+          <div
+            className={clsx(
+              classes.imageAnimation,
+              themeMode === 'dark' ? classes.imageAnimationDark: '',
+            )}
+          />
+        )}
+      />
+    </div>
+  );
+};
+
+Hero.propTypes = {
+  /**
+   * External classes
+   */
+  className: PropTypes.string,
+  /**
+   * Theme mode
+   */
+  themeMode: PropTypes.string,
+};
+
+export default Hero;
