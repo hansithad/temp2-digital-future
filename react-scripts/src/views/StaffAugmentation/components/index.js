b3778298750f144951b9ba310e34bec73222077f
hansithad <na5703hansitha@gmail.com>
2021-02-15 19:28:13 +0530

    remove unnecessary sections from team page

--- react-scripts/src/views/StaffAugmentation/components/index.js
+++ react-scripts/src/views/StaffAugmentation/components/index.js
@@ -1,5 +1,4 @@
 export { default as About } from './About';
-export { default as AboutBottom } from './AboutBottom';
 export { default as Features } from './Features';
 export { default as Hero } from './Hero';
 export { default as Jobs } from './Jobs';
