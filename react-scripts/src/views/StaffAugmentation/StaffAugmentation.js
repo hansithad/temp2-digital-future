87ce19c4a38b34e8133c675d9a39d55ece1c82ff
hansithad <na5703hansitha@gmail.com>
2021-02-24 17:28:18 +0530

    add contact form icon in staff page

--- react-scripts/src/views/StaffAugmentation/StaffAugmentation.js
+++ react-scripts/src/views/StaffAugmentation/StaffAugmentation.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Section, SectionAlternate } from 'components/organisms';
+import { Section, SectionAlternate, ContactForm } from 'components/organisms';
 import {
   About,
   Features,
@@ -13,6 +13,8 @@ import {
   features,
   team,
 } from './data';
+import {AppBar, Divider, Drawer, IconButton, Toolbar} from "@material-ui/core";
+import ForumIcon from "@material-ui/icons/Forum";
 
 const useStyles = makeStyles(theme => ({
   sectionTrucking: {
@@ -32,10 +34,49 @@ const useStyles = makeStyles(theme => ({
   aboutSection: {
     background: '#0c133e',
   },
+  appBarBottom: {
+    top: 'auto',
+    bottom: 0,
+    background: 'transparent',
+    boxShadow: 'none',
+  },
+  chatIconButton: {
+    position: 'absolute',
+    right: theme.spacing(3),
+    left: 'auto',
+    top: theme.spacing(-3),
+    background: theme.palette.primary.main,
+    width: 55,
+    height: 55,
+    boxShadow: `0 2px 10px 0 ${theme.palette.cardShadow}`,
+    '&:hover': {
+      background: theme.palette.primary.main,
+    },
+  },
+  contactForm: {
+    padding: theme.spacing(3, 2),
+    maxWidth: 800,
+    margin: '0 auto',
+  },
+  forumIcon: {
+    color: 'white',
+    width: 25,
+    height: 25,
+  },
 }));
 
 const StaffAugmentation = () => {
   const classes = useStyles();
+
+  const [openBottombar, setOpenBottombar] = React.useState(false);
+
+  const handleBottombarOpen = () => {
+    setOpenBottombar(true);
+  };
+
+  const handleBottombarClose = () => {
+    setOpenBottombar(false);
+  };
 
   return (
     <div>
@@ -54,6 +95,26 @@ const StaffAugmentation = () => {
       <Section>
         <Search />
       </Section>
+      <Divider />
+      <AppBar position="fixed" className={classes.appBarBottom}>
+        <Toolbar disableGutters className={classes.toolbarBottom}>
+          <IconButton
+            className={classes.chatIconButton}
+            onClick={handleBottombarOpen}
+          >
+            <ForumIcon className={classes.forumIcon} />
+          </IconButton>
+          <Drawer
+            anchor="bottom"
+            open={openBottombar}
+            onClose={handleBottombarClose}
+          >
+            <div className={classes.contactForm}>
+              <ContactForm />
+            </div>
+          </Drawer>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 };
