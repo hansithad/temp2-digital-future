3b8c76be71e813012966ccfcd169a2efa361509a
hansithad <na5703hansitha@gmail.com>
2021-03-10 20:16:03 +0530

    add contact form functionality to other 2 pages

--- react-scripts/src/views/CustomCareerListingMinimal/CustomCareerListingMinimal.js
+++ react-scripts/src/views/CustomCareerListingMinimal/CustomCareerListingMinimal.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {AppBar, Divider, Drawer, IconButton, Toolbar} from '@material-ui/core';
-import { Section, SectionAlternate,ContactForm } from 'components/organisms';
+import {AppBar, Backdrop, CircularProgress, Divider, Drawer, IconButton, Snackbar, Toolbar} from '@material-ui/core';
+import { Section, SectionAlternate,CustomContactForm } from 'components/organisms';
 import { Articles,Teaser, Features, Hero, CustomJobs, Newsletter } from './components';
 
 import { aricles, features, jobs } from './data';
 import ForumIcon from "@material-ui/icons/Forum";
 import {makeStyles} from "@material-ui/core/styles";
+import MuiAlert from "@material-ui/lab/Alert";
 
 const useStyles = makeStyles(theme => ({
   appBarBottom: {
@@ -42,13 +43,23 @@ const useStyles = makeStyles(theme => ({
     maxWidth: 800,
     margin: '0 auto',
   },
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: '#fff',
+  }
 }));
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 
 const CustomCareerListingMinimal = () => {
 
   const classes = useStyles();
 
   const [openBottombar, setOpenBottombar] = React.useState(false);
+  const [openSnack, setOpenSnack] = React.useState(false);
+  const [openLoader, setOpenLoader] = React.useState(false);
 
   const handleBottombarOpen = () => {
     setOpenBottombar(true);
@@ -57,6 +68,44 @@ const CustomCareerListingMinimal = () => {
   const handleBottombarClose = () => {
     setOpenBottombar(false);
   };
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpenSnack(false);
+  };
+
+  const postSubmission = (formValues)=>{
+
+    setOpenLoader(true);
+    handleBottombarClose();
+    postData('https://script.google.com/macros/s/AKfycbyTT3r288vZfGaeKGRON7SUZk1HLZdJssRgqfrKvLCou4yPkzrTf8hpGzviT30AI033/exec',
+      formValues)
+      .then(data => {
+        console.log(data); // JSON data parsed by `data.json()` call
+        setOpenLoader(false);
+        setOpenSnack(true);
+      });
+  };
+
+  async function postData(url = '', data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: 'POST',
+      mode: 'cors',
+      cache: 'no-cache',
+      credentials: 'omit',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow',
+      referrerPolicy: 'no-referrer',
+      body: new URLSearchParams(data)
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+  }
 
   return (
     <div>
@@ -89,12 +138,19 @@ const CustomCareerListingMinimal = () => {
             onClose={handleBottombarClose}
           >
             <div className={classes.contactForm}>
-              <ContactForm />
+              <CustomContactForm postSubmission={postSubmission} />
             </div>
           </Drawer>
         </Toolbar>
       </AppBar>
-
+      <Snackbar open={openSnack} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success">
+          Your feedback saved successfully
+        </Alert>
+      </Snackbar>
+      <Backdrop className={classes.backdrop} open={openLoader} >
+        <CircularProgress color="inherit" />
+      </Backdrop>
     </div>
 
   )
